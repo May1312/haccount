c07a649b6829ffe1e4a4a6eedf75d0a9032ab2b7
15501233770@163.com <15501233770@163.com>
2018-08-22 17:20:55 +0800

    编辑用户信息 头像url替换

---
+++ src/main/java/com/fnjz/constants/DomainEnum.java
@@ -0,0 +1,39 @@
+package com.fnjz.constants;
+
+/**
+ * 域名枚举类
+ * Created by yhang on 2018/8/22.
+ */
+public enum DomainEnum {
+
+    //七牛云上传域名---->用户头像
+    HEAD_PICTURE_DOMAIN("head-picture","http://head.image.fengniaojizhang.cn/"),
+
+    //七牛云上传域名---->记账类目
+    LABEL_PICTURE_DOMAIN("label-picture","http://label.image.fengniaojizhang.cn/");
+
+    private String domainName;
+
+    private String domainUrl;
+
+    DomainEnum(String domainName, String domainUrl) {
+        this.setDomainName(domainName);
+        this.setDomainUrl(domainUrl);
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getDomainUrl() {
+        return domainUrl;
+    }
+
+    public void setDomainUrl(String domainUrl) {
+        this.domainUrl = domainUrl;
+    }
+}
