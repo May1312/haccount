846c556bfd25e56d94a25e5809493485ea0f3cf7
15501233770@163.com <15501233770@163.com>
2018-10-25 17:31:51 +0800

    修改注册流程  更新微信头像

--- src/main/java/com/fnjz/front/utils/ShareCodeUtil.java
+++ src/main/java/com/fnjz/front/utils/ShareCodeUtil.java
@@ -214,7 +214,7 @@ public class ShareCodeUtil {
     public void  main(){
         String str2 = id2sharecode(3411);
         System.out.println(str2);
-        int str3 = sharecode2id("99995048");
+        int str3 = sharecode2id("99991048");
         System.out.println(str3);
     }
 
