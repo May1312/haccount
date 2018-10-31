b0db2002fb1f79543a771e0446f571db6a33a0e3
15501233770@163.com <15501233770@163.com>
2018-10-31 11:09:54 +0800

    修改首页弹框sql加入版本为空判断

--- src/main/java/com/fnjz/front/dao/HomeWindowRestDao.java
+++ src/main/java/com/fnjz/front/dao/HomeWindowRestDao.java
@@ -19,10 +19,10 @@ public interface HomeWindowRestDao {
      * @return
      */
     @ResultType(HomeWindowRestDTO.class)
-    @Sql("SELECT * FROM hbird_home_window WHERE STATUS = 1 AND IF ( uptime IS NULL, 1 = 1, uptime <= CURRENT_TIMESTAMP ) AND IF ( uptime IS NULL, 1 = 1, downtime >= CURRENT_TIMESTAMP ) AND IF ( :type = 'android' and :version is not null, android_show_version <=:version, 1 = 1 ) AND IF ( :type = 'ios' and :version is not null, ios_show_version <=:version, 1 = 1 ) ORDER BY priority,create_date;")
+    @Sql("SELECT * FROM hbird_home_window WHERE STATUS = 1 AND IF ( uptime IS NULL, 1 = 1, uptime <= CURRENT_TIMESTAMP ) AND IF ( downtime IS NULL, 1 = 1, downtime >= CURRENT_TIMESTAMP ) AND IF ( : type = 'android' AND : version IS NOT NULL, android_show_version IS NOT NULL and trim(android_show_version)!='' AND android_show_version <=: version, 1 = 1 ) AND IF ( : type = 'ios' AND : version IS NOT NULL, ios_show_version IS NOT NULL and trim(ios_show_version)!='' AND ios_show_version <=: version, 1 = 1 ) AND IF ( : type = 'wxapplet', smallprogram_show_version IS NOT NULL and trim(smallprogram_show_version)!='', 1 = 1 ) ORDER BY priority, create_date;")
     List<HomeWindowRestDTO> listForWindow(@Param("type") String type,@Param("version") String version);
 
     @ResultType(BannerRestDTO.class)
-    @Sql("select * from hbird_banner where status=1 and if(uptime is null,1=1,uptime<=CURRENT_TIMESTAMP) and if(uptime is null,1=1,downtime>=CURRENT_TIMESTAMP) order by priority,create_date;")
+    @Sql("select * from hbird_banner where status=1 and if(uptime is null,1=1,uptime<=CURRENT_TIMESTAMP) and if(downtime is null,1=1,downtime>=CURRENT_TIMESTAMP) order by priority,create_date;")
     List<BannerRestDTO> listForSlideShow();
 }
