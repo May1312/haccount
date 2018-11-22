4af9fc7d2b0f8f402c92f4b6a7056133541289fc
zyh <496527068@qq.com>
2018-11-22 14:22:54 +0800

    注册多加手机号修改

--- src/main/java/com/fnjz/front/service/impl/api.userinfo/UserInfoRestServiceImpl.java
+++ src/main/java/com/fnjz/front/service/impl/api.userinfo/UserInfoRestServiceImpl.java
@@ -234,7 +234,7 @@ public class UserInfoRestServiceImpl extends CommonServiceImpl implements UserIn
         }
 
         //判断是否是从邀请多人记账页面过来，如果是带手机号  设置手机号
-        if(StringUtils.isNotEmpty(jsonObject.get("mobile").toString())){
+        if(StringUtils.isNotEmpty(jsonObject.getString("mobile"))){
             userInfoRestEntity.setMobile(map.get("mobile"));
         }
 
@@ -246,7 +246,7 @@ public class UserInfoRestServiceImpl extends CommonServiceImpl implements UserIn
         userLogin.setWechatAuth(userInfoRestEntity.getWechatAuth());
         userLogin.setUserInfoId(insertId);
         //判断是否是从邀请多人记账页面过来，如果是带手机号  设置手机号
-        if(StringUtils.isNotEmpty(jsonObject.get("mobile").toString())){
+        if(StringUtils.isNotEmpty(jsonObject.getString("mobile"))){
             userLogin.setMobile(map.get("mobile"));
         }
         userLoginRestDao.insert(userLogin);
