731e5212909eef2b5e14a3f713757efdae4890da
15501233770@163.com <15501233770@163.com>
2018-11-28 17:08:07 +0800

    修改签到问题

--- src/main/java/com/fnjz/front/utils/ShareCodeUtil.java
+++ src/main/java/com/fnjz/front/utils/ShareCodeUtil.java
@@ -212,11 +212,11 @@ public class ShareCodeUtil {
 
     @Test
     public void  main(){
-        String str2 = id2sharecode(3433);
+        String str2 = id2sharecode(198);
         System.out.println(str2);
         int str3 = sharecode2id("99977630");
         System.out.println(str3);
-        int str4 = sharecode2id("99993433");
+        int str4 = sharecode2id("99993459");
         System.out.println(str4);
     }
 
